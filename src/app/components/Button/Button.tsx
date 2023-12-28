fc2cf250cc12e4f41f036e941a468fa1ed97858b
Lucas <pilatilucas@gmail.com>
2023-12-28 18:42:42 -0300

    Update Button.tsx

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,9 +1,12 @@
 
 import './Button.css'
 
-export default function Button({ title }) {
+interface ButtonProps {
+    title: string;
+}
+
+export default function Button({ title }: ButtonProps) {
     
- 
 
     return (
         <>
