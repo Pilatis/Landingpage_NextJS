32f88bbe08b93c9df97409df32cf3574f2e0c59f
Lucas <pilatilucas@gmail.com>
2023-12-29 16:53:38 -0300

    Update Input.tsx

--- src/app/components/Input/Input.tsx
+++ src/app/components/Input/Input.tsx
@@ -1,12 +1,19 @@
 
 import './Input.css'
 
+interface InputProps {//definindo a interface que descreve os tipos para as props
+  type: string;
+  placeholder: string;
+  pattern?: string;
+  required: boolean;
+}
+
 export default function Input({
   type,
   placeholder,
   pattern,
   required
-}) {
+} : InputProps) {//Para o typescript reconhecer os tipos esperados pelas props
 
     return (
        <input 
